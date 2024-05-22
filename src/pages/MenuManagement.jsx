a9261efbc23e84f4fccc890f9edbcb78d85e5412
Rachael Njoroge <wangoinjoroge450@gmail.com>
2024-05-22 16:54:14 +0300

    Finished one item on menu management page

--- src/pages/MenuManagement.jsx
+++ src/pages/MenuManagement.jsx
@@ -2,11 +2,18 @@ import React from 'react'
 import Buttons from '../components/layout/Buttons'
 import Aside from '../components/vendors dashboard/Aside'
 import VendorsDashboardNavbar from '../components/vendors dashboard/VendorsDashboardNavbar'
+import { FaEdit, FaPlus, FaRegTrashAlt, FaToggleOn } from 'react-icons/fa'
 
 function MenuManagement() {
   const menuManagementBtn = {
     backgroundColor: "#006B61",
-    border: "none"
+    border: "none",
+    marginRight: "12px"
+  }
+  const menuManagement = {
+    color: "#006B61",
+    border: "1px solid #006B61",
+    backgroundColor: "white"
   }
   return (
     <>
@@ -15,15 +22,130 @@ function MenuManagement() {
       <div id='menuManagement'>
         <div className='menuTitle'>
           <h4>Menu Management</h4> 
-          <Buttons style={menuManagementBtn}>Add Food</Buttons><Buttons style={menuManagementBtn}>Edit</Buttons>
-        </div>
-        {/* <div>
-            <h4>Menu Food</h4>
-            <h4>Menu Categories</h4>
-          </div>
           <div>
+            <Buttons style={menuManagementBtn}> <FaPlus></FaPlus> Add Food</Buttons><Buttons style={menuManagement}> <FaEdit></FaEdit> Edit</Buttons>
+          </div>
+        </div>
+        <hr  />
+        <div className='chooseTab'>
+          <h6>Menu Food</h6>
+          <h6>Menu Categories</h6>
+        </div>
+        <hr />
+        <div className='menuDetails'>
+          <div>
+            <div className='menuEntry'>
+              <h6>Rank</h6>
+              <h6>Image</h6>
+              <h6>Name</h6>
+              <h6>Category</h6>
+              <h6>Price</h6>
+              <h6>Available</h6>
+              <h6>Action</h6>
+            </div>
+            <hr />
+            <div className='menuItem'>
+              <p>1</p>
+              <img src="\images\menumanagementfoodimage.png" alt="food image" />
+              <p>Egusi soup</p>
+              <p>Soup</p>
+              <p>1,500</p>
+              <FaToggleOn className='menuIcons'></FaToggleOn>
+              <span><div><FaEdit className='menuIcon'></FaEdit><FaRegTrashAlt className='menuIcon'></FaRegTrashAlt></div></span>
+            </div>
+            <hr />
+          </div>
+          {/* <div className='menuEntry'>
+            <h6>Rank</h6>
+            <br />
+            <p>1</p>
+            <br />
+            <hr />
+            <p>2</p>
+            <p>3</p>
+            <p>4</p>
+            <p>5</p>
+            <p>6</p>
+            <p>7</p>
+            <p>8</p>
+            <p>9</p>
+            <p>10</p>
+          </div> */}
+          {/* <div>
+            <h6>Image</h6>
+            <div className='menuManagementImage'>
+              <img src="\images\menumanagementfoodimage.png" alt="food image" />
+              <img src="\images\menumanagementfoodimage.png" alt="food image" />
+              <img src="\images\menumanagementfoodimage.png" alt="food image" />
+              <img src="\images\menumanagementfoodimage.png" alt="food image" />
+              <img src="\images\menumanagementfoodimage.png" alt="food image" />
+              <img src="\images\menumanagementfoodimage.png" alt="food image" />
+              <img src="\images\menumanagementfoodimage.png" alt="food image" />
+              <img src="\images\menumanagementfoodimage.png" alt="food image" />
+              <img src="\images\menumanagementfoodimage.png" alt="food image" />
+              <img src="\images\menumanagementfoodimage.png" alt="food image" />
+            </div>
+          </div> */}
+          {/* <div className='menuEntry1'>
+            <h6>Name</h6>
+            <br />
+            <p>Egusi soup</p>
+            <br />
+            <p>Afang soup</p>
+            <p>Pounded yam</p>
+            <p>Ofada Rice</p>
+            <p>Coconut rice</p>
+            <p>Jollof Rice</p>
+            <p>Ogbono soup</p>
+            <p>Ofe Nsala</p>
+            <p>Banga Soup</p>
+            <p>Eba</p>
+          </div> */}
+          {/* <div className='menuEntry'>
+            <h6>Category</h6>
+            <br />
+            <p>Soup</p>
+            <br />
+            <p>Soup</p>
+            <p>Swallow</p>
+            <p>Rice</p>
+            <p>Rice</p>
+            <p>Rice</p>
+            <p>Soup</p>
+            <p>Soup</p>
+            <p>Soup</p>
+            <p>Swallow</p>
+          </div> */}
+          {/* <div className='menuEntry'>
+            <h6>Price</h6>
+            <br />
+            <p>1,500</p>
+            <br />
+            <p>2,000</p>
+            <p>3,000</p>
+            <p>2,500</p>
+            <p>3,500</p>
+            <p>2,000</p>
+            <p>2,000</p>
+            <p>3,000</p>
+            <p>2,500</p>
+            <p>2,500</p>
+          </div> */}
+          {/* <div className='menuEntry'>
+            <h6>Available</h6>
+            <br />
+            <FaToggleOn className='menuIcons'></FaToggleOn>
+            <br />
+            <FaToggleOff className='menuIcons'></FaToggleOff>
             
-        </div> */}
+          </div> */}
+          {/* <div className='menuEntry'>
+            <h6>Action</h6>
+            <br />
+            <span><div><FaEdit className='menuIcon'></FaEdit><FaRegTrashAlt className='menuIcon'></FaRegTrashAlt></div></span>
+            <br /> 
+          </div> */}
+        </div>
       </div>
     </>
   )
