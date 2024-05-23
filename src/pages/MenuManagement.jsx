4ab44eaadca088d80fde44a3ab7f4f6e58946dbe
Rachael Njoroge <wangoinjoroge450@gmail.com>
2024-05-23 20:05:01 +0300

    Finished the menu management component

--- src/pages/MenuManagement.jsx
+++ src/pages/MenuManagement.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import Buttons from '../components/layout/Buttons'
 import Aside from '../components/vendors dashboard/Aside'
 import VendorsDashboardNavbar from '../components/vendors dashboard/VendorsDashboardNavbar'
-import { FaEdit, FaPlus, FaRegTrashAlt, FaToggleOn } from 'react-icons/fa'
+import { FaEdit, FaPlus, FaRegTrashAlt, FaToggleOff, FaToggleOn } from 'react-icons/fa'
 
 function MenuManagement() {
   const menuManagementBtn = {
@@ -50,10 +50,99 @@ function MenuManagement() {
               <p>Egusi soup</p>
               <p>Soup</p>
               <p>1,500</p>
-              <FaToggleOn className='menuIcons'></FaToggleOn>
-              <span><div><FaEdit className='menuIcon'></FaEdit><FaRegTrashAlt className='menuIcon'></FaRegTrashAlt></div></span>
+              <FaToggleOn className='menuOn'></FaToggleOn>
+              <span ><span className='menuInactive'><FaEdit></FaEdit></span><span className='menuActive'><FaRegTrashAlt></FaRegTrashAlt></span></span>
             </div>
             <hr />
+            <div className='menuItem'>
+              <p>2</p>
+              <img src="\images\menumanagementfoodimage.png" alt="food image" />
+              <p>Afang soup</p>
+              <p>Soup</p>
+              <p>2,000</p>
+              <FaToggleOff className='menuOff'></FaToggleOff>
+              <span><span className='menuInactive'><FaEdit></FaEdit></span><span className='menuActive'><FaRegTrashAlt></FaRegTrashAlt></span></span>
+            </div>
+            <hr />
+            <div className='menuItem'>
+              <p>3</p>
+              <img src="\images\menumanagementfoodimage.png" alt="food image" />
+              <p>Pounded yam</p>
+              <p>Swallow</p>
+              <p>3,000</p>
+              <FaToggleOff className='menuOff'></FaToggleOff>
+              <span><span className='menuActive'><FaEdit></FaEdit></span><span className='menuInactive'><FaRegTrashAlt></FaRegTrashAlt></span></span>
+            </div>
+            <hr />
+            <div className='menuItem'>
+              <p>4</p>
+              <img src="\images\menumanagementfoodimage.png" alt="food image" />
+              <p>Ofada Rice</p>
+              <p>Rice</p>
+              <p>2,500</p>
+              <FaToggleOn className='menuOn'></FaToggleOn>
+              <span><span className='menuActive'><FaEdit></FaEdit></span><span className='menuInactive'><FaRegTrashAlt></FaRegTrashAlt></span></span>
+            </div>
+            <hr />
+            <div className='menuItem'>
+              <p>5</p>
+              <img src="\images\menumanagementfoodimage.png" alt="food image" />
+              <p>Coconut Rice</p>
+              <p>Rice</p>
+              <p>3,500</p>
+              <FaToggleOn className='menuOn'></FaToggleOn>
+              <span><span className='menuActive'><FaEdit></FaEdit></span><span className='menuInactive'><FaRegTrashAlt></FaRegTrashAlt></span></span>
+            </div>
+            <hr />
+            <div className='menuItem'>
+              <p>6</p>
+              <img src="\images\menumanagementfoodimage.png" alt="food image" />
+              <p>Jollof Rice</p>
+              <p>Rice</p>
+              <p>2,000</p>
+              <FaToggleOn className='menuOn'></FaToggleOn>
+              <span><span className='menuInactive'><FaEdit></FaEdit></span><span className='menuActive'><FaRegTrashAlt></FaRegTrashAlt></span></span>
+            </div>
+            <hr />
+            <div className='menuItem'>
+              <p>7</p>
+              <img src="\images\menumanagementfoodimage.png" alt="food image" />
+              <p>Ogbono soup</p>
+              <p>Soup</p>
+              <p>2,000</p>
+              <FaToggleOff className='menuOff'></FaToggleOff>
+              <span><span className='menuInactive'><FaEdit></FaEdit></span><span className='menuActive'><FaRegTrashAlt></FaRegTrashAlt></span></span>
+            </div>
+            <hr />
+            <div className='menuItem'>
+              <p>8</p>
+              <img src="\images\menumanagementfoodimage.png" alt="food image" />
+              <p>Ofe Nsala</p>
+              <p>Soup</p>
+              <p>3,500</p>
+              <FaToggleOn className='menuOn'></FaToggleOn>
+              <span><span className='menuInactive'><FaEdit></FaEdit></span><span className='menuActive'><FaRegTrashAlt></FaRegTrashAlt></span></span>
+            </div>
+            <hr />
+            <div className='menuItem'>
+              <p>9</p>
+              <img src="\images\menumanagementfoodimage.png" alt="food image" />
+              <p>Banga Soup</p>
+              <p>Soup</p>
+              <p>2,500</p>
+              <FaToggleOn className='menuOn'></FaToggleOn>
+              <span><span className='menuInactive'><FaEdit></FaEdit></span><span className='menuActive'><FaRegTrashAlt></FaRegTrashAlt></span></span>
+            </div>
+            <hr />
+            <div className='menuItem'>
+              <p>10</p>
+              <img src="\images\menumanagementfoodimage.png" alt="food image" />
+              <p>Eba</p>
+              <p>Swallow</p>
+              <p>2,500</p>
+              <FaToggleOn className='menuOn'></FaToggleOn>
+              <span><span className='menuInactive'><FaEdit></FaEdit></span><span className='menuActive'><FaRegTrashAlt></FaRegTrashAlt></span></span>
+            </div>
           </div>
           {/* <div className='menuEntry'>
             <h6>Rank</h6>
